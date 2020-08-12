6b0ae1fb4a475111f4a45d3b465a49cfac6555d0
Jayshri Patil <dollipatil13j@gmail.com>
2020-08-12 13:04:28 +0530

    Last Commit

--- Register/App_Start/RouteConfig.cs
+++ Register/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Register
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Register", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
